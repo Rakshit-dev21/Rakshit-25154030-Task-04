e356b06c4cd8be2fe5f632781fd27c9d7f1ce72e
rakshit <rajputraksit571@gmail.com>
2026-09-20 23:05:06 +0530

    added the lose screen and restart button

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ energy = 150
 selected = None
 font = pygame.font.Font(None , 30)
 small_font = pygame.font.Font(None , 22)
+big_font = pygame.font.Font(None , 50)
 shooter_btn = pygame.Rect((40 , 20) , (100 , 60))
 producer_btn = pygame.Rect((150 , 20) , (100 , 60))
 wall_btn = pygame.Rect((260 , 20) , (100 , 60))
@@ -93,44 +94,53 @@ zombie_spawned = 0
 total_zombie = 15
 running = True
 while running:
+    for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN and state == 'playing':
+                    clicked_btn = False
+                    if event.button == 1:
+                        for button in buttons:
+                            if button[0].collidepoint(event.pos):
+                                selected = button[1]
+                                clicked_btn = True
+                        if not clicked_btn:
+                            mouse_x , mouse_y = event.pos
+                            print(mouse_x , mouse_y)
+                            c = (mouse_x - grid_start_x) // cell_size
+                            r = (mouse_y - grid_start_y) // cell_size
+                            print(r , c)
+                            if(0 <= r < row and 0 <= c < col):
+                                if selected is not None:
+                                    occupied = False
+                                    for d in defenders:
+                                        if d.row == r and d.col == c:
+                                            occupied = True
+                                            break
+                                    if not occupied:
+                                        new_defender = None
+                                        if selected == 'shooter':
+                                            new_defender = Shooter(r , c)
+                                        elif selected == 'producer':
+                                            new_defender = Producer(r , c)
+                                        elif selected == 'wall':
+                                            new_defender = Wall(r , c)
+    
+                                        if new_defender is not None and energy >= new_defender.cost:
+                                            defenders.append(new_defender)
+                                            energy -= new_defender.cost
+                elif event.type == pygame.KEYDOWN:
+                    if state == 'lose' and event.key == pygame.K_SPACE:
+                        state = 'playing'
+                        defenders = []
+                        bullets = []
+                        zombies = []
+                        energy = 150 
+                        spawn_timer = 0
+                        spawn_interval = 400
+                        selected = None
     if state == 'playing':
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                clicked_btn = False
-                if event.button == 1:
-                    for button in buttons:
-                        if button[0].collidepoint(event.pos):
-                            selected = button[1]
-                            clicked_btn = True
-                    if not clicked_btn:
-                        mouse_x , mouse_y = event.pos
-                        print(mouse_x , mouse_y)
-                        c = (mouse_x - grid_start_x) // cell_size
-                        r = (mouse_y - grid_start_y) // cell_size
-                        print(r , c)
-                        if(0 <= r < row and 0 <= c < col):
-                            if selected is not None:
-                                occupied = False
-                                for d in defenders:
-                                    if d.row == r and d.col == c:
-                                        occupied = True
-                                        break
-                                if not occupied:
-                                    new_defender = None
-                                    if selected == 'shooter':
-                                        new_defender = Shooter(r , c)
-                                    elif selected == 'producer':
-                                        new_defender = Producer(r , c)
-                                    elif selected == 'wall':
-                                        new_defender = Wall(r , c)
-
-                                    if new_defender is not None and energy >= new_defender.cost:
-                                        defenders.append(new_defender)
-                                        energy -= new_defender.cost
-
         spawn_timer += 1
         if spawn_timer >= spawn_interval :
             spawn_timer = 0
@@ -168,6 +178,9 @@ while running:
         bullets = [b for b in bullets if b.rect.x < 800]
         zombies = [z for z in zombies if z.health > 0]
         defenders = [d for d in defenders if d.health > 0 ]
+        for z in zombies:
+            if z.rect.x < grid_start_x:
+                state = 'lose'
     screen.fill((30 , 30 ,30))
     for i in range(row):
         for j in range(col):
@@ -198,6 +211,13 @@ while running:
 
     text_surface = font.render(f" Energy : {energy}" , True , (255 , 255 , 255))
     screen.blit(text_surface , (600 , 40))
+    if state == 'lose':
+        lose_text = big_font.render("YOU LOSE" , True , (255 , 0 , 0))
+        lose_rect = lose_text.get_rect(center = (400 , 300))
+        restart_text = font.render("press Space to restart" ,True , (255 , 255 , 255))
+        restart_rect = restart_text.get_rect(center = (400 , 360))
+        screen.blit(lose_text , lose_rect)
+        screen.blit(restart_text , restart_rect)
     pygame.display.update()
     clock.tick(60)
 
